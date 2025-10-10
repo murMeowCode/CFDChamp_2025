6bc756f22c918bac5162618f4f262e4dc7faa260
murMeowCode <grisha23.01.2004@gmail.com>
2025-10-10 16:12:54 +0300

    update gitignore

--- shared/database/database.py
+++ shared/database/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer
 from shared.config.base import settings
 import logging
 
