b7022300c61b205412cf46fb20e1e9b97c1594a7
murMeowCode <grisha23.01.2004@gmail.com>
2025-10-11 12:31:20 +0300

    refactor auth util, replace to shared

--- backend/profile_service/api/endpoints/profiles.py
+++ backend/profile_service/api/endpoints/profiles.py
@@ -1,42 +1,45 @@
+"""апи для работы с профилями"""#pylint: disable=E0401, E0611
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from auth_service.messaging.producers import AuthProducer
-from profile_service.services.auth import AuthDependency
+from shared.utils.auth_utils import AuthDependency
+from shared.database.database import get_db
 from profile_service.services.controller import ProfileController
 from profile_service.schemas.profile import ProfileResponse, ProfileUpdate
-from shared.database.database import get_db
-import uuid
-from typing import List
+
+
 
 router = APIRouter()
 
 def get_auth_dependency(producer: AuthProducer = Depends(lambda: AuthDependency.producer)):
+    """получение продюсера для проверки прав доступа"""
     return AuthDependency(producer)
 
-# Эндпоинт для получения своего профиля
 @router.get("/me", response_model=ProfileResponse)
 async def get_my_profile(
     user: dict = Depends(get_auth_dependency().get_current_user),
     db: AsyncSession = Depends(get_db)
 ):
+    """получение своего профиля"""
     controller = ProfileController(db)
     return await controller.get_profile(user["user_id"])
 
-# Эндпоинт для обновления своего профиля
 @router.put("/me", response_model=ProfileResponse)
 async def update_my_profile(
     profile_data: ProfileUpdate,
     user: dict = Depends(get_auth_dependency().get_current_user),
     db: AsyncSession = Depends(get_db)
 ):
+    """Эндпоинт для обновления своего профиля"""
     controller = ProfileController(db)
     return await controller.update_profile(user["user_id"], profile_data)
 
-# Эндпоинт для получения всех пользователей (только для role=2)
 @router.get("/all", response_model=List[ProfileResponse])
 async def get_all_users(
-    user: dict = Depends(lambda: get_auth_dependency().require_role(2)),
+    _: dict = Depends(lambda: get_auth_dependency().require_role(2)),
     db: AsyncSession = Depends(get_db)
 ):
+    """Эндпоинт для получения всех пользователей (только для role=2)"""
     controller = ProfileController(db)
     return await controller.get_all_profiles()
