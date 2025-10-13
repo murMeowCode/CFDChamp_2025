b1265ecbc9f942cbc8159400d0ed6870cbf6d9a5
murMeowCode <grisha23.01.2004@gmail.com>
2025-10-13 16:55:11 +0300

    fix table uuids

--- backend/mailing_service/models/message.py
+++ backend/mailing_service/models/message.py
@@ -1,5 +1,5 @@
 """модели сообщений"""#pylint: disable=E0401,E1123
-import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, String, Boolean, Text
 from shared.database.database import Base
 
@@ -7,8 +7,8 @@ class Message(Base):
     """модель сообщения"""
     __tablename__ = "messages"
 
-    id = Column(uuid.UUID(as_uuid=True), primary_key=True)
-    user_id = Column(uuid.UUID(as_uuid=True), nullable=False, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), nullable=False, index=True)
     subject = Column(String(255), nullable=False)  # Тема письма
     content = Column(Text, nullable=False)  # Текст письма
     is_read = Column(Boolean, default=False)  # Статус прочтения
--- backend/profile_service/models/profile.py
+++ backend/profile_service/models/profile.py
@@ -1,6 +1,6 @@
 """объявления моделей"""#pylint: disable=E0401, E0611, E1123
 from datetime import date
-import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, String
 from shared.database.database import Base
 
@@ -8,7 +8,7 @@ class Profile(Base):
     """модель профиля"""
     __tablename__ = "profiles"
 
-    user_id = Column(uuid.UUID(as_uuid=True),
+    user_id = Column(UUID(as_uuid=True),
                       primary_key=True, index=True, nullable=False)
     first_name = Column(String(100))
     last_name = Column(String(100))
