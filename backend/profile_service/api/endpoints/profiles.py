984e4a798ac5d1e039d765c3dec38944ab19f614
murMeowCode <grisha23.01.2004@gmail.com>
2025-10-14 09:29:38 +0300

    create api for upload avatar

--- backend/profile_service/api/endpoints/profiles.py
+++ backend/profile_service/api/endpoints/profiles.py
@@ -1,11 +1,11 @@
 """апи для работы с профилями"""#pylint: disable=E0401, E0611
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 from shared.utils.auth_utils import get_auth_dependency
 from shared.database.database import get_db
 from profile_service.services.controller import ProfileController
-from profile_service.schemas.profile import ProfileResponse, ProfileUpdate
+from profile_service.schemas.profile import AvatarUploadResponse, ProfileResponse, ProfileUpdate
 
 
 
@@ -29,6 +29,19 @@ async def update_my_profile(
     """Эндпоинт для обновления своего профиля"""
     controller = ProfileController(db)
     return await controller.update_profile(user["user_id"], profile_data)
+
+@router.post("/me/avatar", response_model=AvatarUploadResponse)
+async def upload_avatar(
+    file: UploadFile = File(...),
+    user: dict = Depends(get_auth_dependency().get_current_user),
+    db: AsyncSession = Depends(get_db)
+):
+    """Загрузка аватарки пользователя"""
+    if not file.filename:
+        raise HTTPException(400, "No file provided")
+
+    controller = ProfileController(db)
+    return await controller.upload_avatar(user["user_id"], file)
 
 @router.get("/all", response_model=List[ProfileResponse])
 async def get_all_users(
