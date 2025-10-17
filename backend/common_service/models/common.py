b77d123cc7024fe04f0a25ed838b58b582e30d8a
murMeowCode <grisha23.01.2004@gmail.com>
2025-10-17 17:26:19 +0300

    update alembic

--- backend/common_service/models/common.py
+++ backend/common_service/models/common.py
@@ -6,6 +6,6 @@ from shared.database.database import Base
 class Item(Base):
     """абстрактный класс"""
     __tablename__ = "items"
-    id = Column(UUID(as_uuid=True), primary_key=True, autoincrement=True)
+    id = Column(UUID(as_uuid=True), primary_key=True)
     title = Column(String(255), nullable=False)
     description = Column(String(1000), nullable=True)
