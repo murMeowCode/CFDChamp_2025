707b2d1f330a48108dde72d5028441dd7c877387
DrozdYrich <drozdove912@gmail.com>
2025-10-18 13:23:25 +0300

    api

--- frontend/src/utils/apiUrl/urlApi.js
+++ frontend/src/utils/apiUrl/urlApi.js
@@ -1,9 +1,9 @@
 export const apiLogin = 
-  'http://10.32.237.12:8000'
+  '/api'
 export const apiRegistr = 
-   'http://10.32.237.12:8000'
+   '/api'
 
 export const apiProfilesMe = 
-  'http://10.32.237.12:8001'
+  '/api'
 
-export const apiRefresh = 'http://10.32.237.12:8000'
+export const apiRefresh = '/api'
